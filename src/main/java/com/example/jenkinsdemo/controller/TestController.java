c8328f6e6d88fdc05fbe3b112c31cef2488f4da9
liufanfandev <liufanfandev@163.com>
2021-08-23 22:38:32 +0800

    init

---
+++ src/main/java/com/example/jenkinsdemo/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsdemo.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class TestController{
+
+    @GetMapping("/hello")
+    String hello(){
+        return "hello word";
+    }
+}
